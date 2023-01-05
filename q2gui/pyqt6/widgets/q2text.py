ceb480bbac7a417d144783d28b3d0e1cf216ccae
Andrei Puchko <andrei.puchko@gmx.de>
2023-01-06 00:17:10 +0100

    q2text - TAB

--- q2gui/pyqt6/widgets/q2text.py
+++ q2gui/pyqt6/widgets/q2text.py
@@ -17,6 +17,7 @@ class q2text(QTextEdit, Q2Widget):
     def __init__(self, meta):
         super().__init__(meta)
         self.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding))
+        self.setTabChangesFocus(True)
         self.set_text(meta.get('data'))
 
     def set_text(self, text):
