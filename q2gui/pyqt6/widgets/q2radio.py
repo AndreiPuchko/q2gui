cedff68a25c81de5d498b5d9ec2d748fd41991c5
Andrei Puchko <andrei.puchko@gmx.de>
2024-03-26 00:34:02 +0100

    radio bugs

--- q2gui/pyqt6/widgets/q2radio.py
+++ q2gui/pyqt6/widgets/q2radio.py
@@ -12,10 +12,8 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-import sys
 
-
-from PyQt6.QtWidgets import QFrame, QHBoxLayout, QVBoxLayout, QRadioButton, QSizePolicy, QApplication
+from PyQt6.QtWidgets import QFrame, QHBoxLayout, QVBoxLayout, QRadioButton, QSizePolicy, QGroupBox
 from PyQt6.QtCore import Qt, QEvent
 from PyQt6.QtGui import QKeyEvent
 
@@ -24,14 +22,16 @@ from q2gui.pyqt6.q2widget import Q2Widget
 from q2gui.q2utils import int_
 
 
-class q2radio(QFrame, Q2Widget):
+class q2radio(QGroupBox, Q2Widget):
     def __init__(self, meta):
         super().__init__(meta)
-        self.setLayout(QVBoxLayout() if "v" in meta.get("control") else QHBoxLayout())
+        self._vertical = True if "v" in meta.get("control") else False
+        self.setLayout(QVBoxLayout() if self._vertical else QHBoxLayout())
         self.setSizePolicy(QSizePolicy.Policy.Maximum, QSizePolicy.Policy.Maximum)
         self.layout().setAlignment(q2_align["7"])
         self.layout().setContentsMargins(0, 0, 0, 0)
         self.setObjectName("radio")
+        # self.setFocusPolicy(Qt.FocusPolicy.StrongFocus)
         self.button_list = []
         for item in meta.get("pic", "").split(";"):
             button = q2RadioButton(item, self)
@@ -41,23 +41,6 @@ class q2radio(QFrame, Q2Widget):
         self.button_list[0].setChecked(True)
         if meta.get("data") is not None:
             self.set_text(meta.get("data"))
-
-    def keyPressEvent(self, ev):
-        if ev.key() in [
-            Qt.Key.Key_Down,
-            Qt.Key.Key_Up,
-            Qt.Key.Key_PageDown,
-            Qt.Key.Key_PageUp,
-        ]:
-            ev.ignore()
-        elif ev.key() == Qt.Key.Key_Right:
-            QApplication.sendEvent(self, QKeyEvent(QEvent.Type.KeyPress, Qt.Key.Key_Tab, ev.modifiers()))
-        elif ev.key() == Qt.Key.Key_Left:
-            QApplication.sendEvent(
-                self, QKeyEvent(QEvent.Type.KeyPress, Qt.Key.Key_Tab, Qt.KeyboardModifier.ShiftModifier)
-            )
-        else:
-            super().keyPressEvent(ev)
 
     def can_get_focus(self):
         return True
@@ -91,6 +74,18 @@ class q2radio(QFrame, Q2Widget):
         else:
             return None
 
+    def focus_next_radiobutton(self):
+        index = self.get_current_index()
+        if index < len(self.button_list) - 1:
+            self.button_list[index + 1].setChecked(True)
+            self._focus_in()
+
+    def focus_prev_radiobutton(self):
+        index = self.get_current_index()
+        if index > 0:
+            self.button_list[index - 1].setChecked(True)
+            self._focus_in()
+
     def get_text(self):
         index_list = [x for x in range(len(self.button_list)) if self.button_list[x].isChecked()]
         if index_list:
@@ -109,6 +104,22 @@ class q2RadioButton(QRadioButton):
         self.radio = radio
         self.toggled.connect(self.value_changed)
         self.setContentsMargins(0, 0, 0, 0)
+
+    def keyPressEvent(self, ev: QKeyEvent | None) -> None:
+        if ev.key() == Qt.Key.Key_Down:
+            self.radio.nextInFocusChain().setFocus()
+            ev.ignore()
+        elif ev.key() == Qt.Key.Key_Up:
+            self.radio.previousInFocusChain().setFocus()
+            ev.ignore()
+        elif ev.key() == Qt.Key.Key_Right:
+            self.radio.focus_next_radiobutton()
+            ev.ignore()
+        elif ev.key() == Qt.Key.Key_Left:
+            self.radio.focus_prev_radiobutton()
+            ev.ignore()
+        else:
+            super().keyPressEvent(ev)
 
     def focusInEvent(self, ev):
         self.radio._focus_in()
