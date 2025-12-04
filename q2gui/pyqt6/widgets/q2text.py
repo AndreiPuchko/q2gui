958c74502f25a33c57720124a326a55f8ccbced7
Andrei Puchko <andrei.puchko@gmx.de>
2025-12-04 15:24:52 +0100

    fix(q2text) - linefeed via setHtml

--- q2gui/pyqt6/widgets/q2text.py
+++ q2gui/pyqt6/widgets/q2text.py
@@ -34,7 +34,7 @@ class q2text(QTextEdit, Q2Widget):
         return super().mousePressEvent(e)
 
     def set_text(self, text):
-        self.setPlainText(text)
+        self.setHtml(text.replace("\n", "<br>"))
 
     def get_text(self):
         return f"{self.toPlainText()}"
